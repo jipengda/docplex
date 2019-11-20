806556801afe7ab1d899cfeef9d7a3832b88e6b3
Dai <jipengda@buffalo.edu>
2019-11-20 17:36:05 -0500

    Column

--- Prioritized.py
+++ Prioritized.py
@@ -6,30 +6,35 @@ import math
 # The wait time is really biggest trouble that blocks my way to dig in time constraint issue
 
  # You know, d[i] defines visiting order
-
-N = 5
-nodes = 
+distance =  [[0,2,3,0,0,0],
+             [0,0,4,5,4,0],
+             [0,2,0,6,2,0],
+             [0,0,6,0,3,4],
+             [0,2,0,9,0,7],
+             [0,0,0,0,0,0]]
+timeInfor = [[0,30],
+             [6,14],
+             [12,17],
+             [11,21],
+             [12,22],
+             [0,30]]
+N = 6
+nodes = [(j) for j in range(N)]
+arcs = [(i,j) for i in nodes for j in nodes if i != j]
+mdl = Model('Dual_ride')
+mdl.minimize(mdl.sum( (x[(i,j)] * distance[(i,j)]) for i,j in arcs )) #objective funtion
 for i,j in arcs:
     mdl.add_indicator(x[(i,j)], d[i] + 1 == d[j], name = 'visiting order')
-
-N = 5
-
-k = []
-
 # I will inversely get index of d variable
+solution = mdl.solve(log_output = True)
 for i in range(1, 6):
-    for j in range(1,6):
-        if d[j] == i:
+    for j in range(1, i):
+        if d[j].solution_value == i:
             k.append(j)
 # use k to store index of d variable , which d variable recordes visiting order
 # After this, k list the visiting nodes in order
-
-for i in range(1,6)
-    x[(k[i]
 # starting node 0 is treated seperately
 mdl.add_constraint(x[(0, k[1])]*distance[0, k[1]]<= timeInfor[k[1]][1])
-
-sum = 0
 # after 0, use loop to add constraints
 for i in range(1,6):
     sum = 0
